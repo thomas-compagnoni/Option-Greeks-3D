644171e180481202636b70a273e75dfcc23616ef
thomas_compagnoni <thomas.compagnoni@studbocconi.it>
2023-04-17 16:06:59 +0200

    add google analytics

--- script/app.py
+++ script/app.py
@@ -53,6 +53,35 @@ server = app.server
 app.layout = LAYOUT
 app.title = 'OptionGreeks3D'
 
+app.index_string = '''
+                    <!DOCTYPE html>
+                    <html>
+                        <head>
+                            <!-- Google tag (gtag.js) -->
+                            <script async src="https://www.googletagmanager.com/gtag/js?id=G-S9CGS1ELE9"></script>
+                            <script>
+                              window.dataLayer = window.dataLayer || [];
+                              function gtag(){dataLayer.push(arguments);}
+                              gtag('js', new Date());
+                            
+                              gtag('config', 'G-S9CGS1ELE9');
+                            </script>
+                            {%metas%}
+                            <title>{%title%}</title>
+                            {%favicon%}
+                            {%css%}
+                        </head>
+                        <body>
+                            {%app_entry%}
+                            <footer>
+                                {%config%}
+                                {%scripts%}
+                                {%renderer%}
+                            </footer>
+                        </body>
+                    </html>
+                    '''
+
 
 @app.callback(
     Output(component_id='price_', component_property='style'),
